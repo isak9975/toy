078c47111cab1c7d92fbc676e4a2f9c0fddc82d4
isak9975 <isak9975@gmail.com>
2024-12-05 21:52:59 +0900

    546456

---
+++ src/AppStart.java
@@ -0,0 +1,7 @@
+import view.SeeView;
+
+public class AppStart {
+    public static void main(String[] args) {
+        SeeView.getInstance().mainpage();
+    }
+}
---
+++ src/view/SeeView.java
@@ -0,0 +1,27 @@
+package view;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class SeeView extends JFrame {
+    private static SeeView seeView = new SeeView();
+    private SeeView(){}
+    public static SeeView getInstance(){return seeView;}
+
+    Scanner scan = new Scanner(System.in);
+
+    See(){
+        super("start");
+        set
+    }
+
+    public void mainpage(){
+        boolean result = true;
+        while(result){
+
+
+            System.out.println("선택하세요");
+
+        }
+    }
+}
